80372068a2328109d62fd93fa19fd702574787e2
csfrez <csfrez@163.com>
2020-06-02 17:28:15 +0800

    初始化

---
+++ src/main/java/com/csfrez/demo/DemoLearnNacosApplication.java
@@ -0,0 +1,13 @@
+package com.csfrez.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLearnNacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLearnNacosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csfrez/demo/DemoLearnNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csfrez.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLearnNacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
