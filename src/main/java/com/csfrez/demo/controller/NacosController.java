2c4315160a49b85b42a4c7fe4e0143e9901e816f
csfrez <csfrez@163.com>
2020-06-02 17:42:28 +0800

    调整版本

--- src/main/java/com/csfrez/demo/controller/NacosController.java
+++ src/main/java/com/csfrez/demo/controller/NacosController.java
@@ -1,6 +1,7 @@
 package com.csfrez.demo.controller;
 
 import com.alibaba.nacos.api.config.annotation.NacosValue;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/nacos")
+@Slf4j
 public class NacosController {
 
     @NacosValue(value = "${logging.level.root:INFO}", autoRefreshed = true)
@@ -24,7 +26,9 @@ public class NacosController {
 
     @GetMapping("/count")
     public Integer count(){
-        return jdbcTemplate.queryForObject("select count(1) from sys_log", Integer.class);
+        Integer count = jdbcTemplate.queryForObject("select count(1) from sys_log", Integer.class);
+        log.info("count={}", count);
+        return count;
     }
 
 }
